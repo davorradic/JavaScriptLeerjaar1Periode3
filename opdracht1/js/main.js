90b08e997e5bdbfec7b36fc168fdb4af02d042e4
davorradic <44604843+davorradic@users.noreply.github.com>
2018-11-08 10:07:15 +0100

    opdracht 1 klaar

---
+++ opdracht1/js/main.js
@@ -0,0 +1,32 @@
+var Pa$$word = 'zaad'
+console.log(Pa$$word);
+
+partnersPr%%ogramma = www.zadenonline.nl
+console.log(partnersPr%%ogramma);
+//er zijn meerdere webshops online met partners programma.
+partnersProgramma = "www.zadenonline.nl";
+
+partners_programma = 'www.zadenonline.nl';
+
+console.log(partnersProgramma + + partners_programma);
+console.log(partners_programma + "  : " + partners_programma);
+
+tracking_code = 11233;
+tracking_code = 11234;
+tracking_code = false;
+tracking_code = '';
+
+console.log("tracking_code is: '" + tracking_code + "' of 11233 of 11234 of iets anders?");
+
+
+//Opdracht1: zoek online minstes 5 affiliate programmas voor verkoop van zaden!
+//zet elk webshop in een variabele
+//print elk variabele uit onder h3 kopje "Project Details" in een li
+document.getElementById("zadenonline-nl").innerHTML = "zadenonline.nl";
+
+/* Opdracht2: meld je aan voor elk gevonden affliate programma en zoek je affiliate link
+als je hebt gevonden plaats het op het zlefde manier naast shop je affliate link in een
+*/
+
+/* Opdracht3: Add logo van een bedrijf naast link
+*/
