8ed640df1146eebcd4ee142f0cf79efbaf7b69e3
davorradic <44604843+davorradic@users.noreply.github.com>
2018-12-01 15:25:55 +0100

    Update main.js

--- opdracht3/js/main.js
+++ opdracht3/js/main.js
@@ -1,7 +1,7 @@
 /* opdracht 3
 Bestudeer hoofdstuk 4 Functies Arrays Objects */
 
-/* Opdracht 1
+/*
 Casus: Je hebt 5 onderdelen bij een product:
 1. Logo van bedrijf (je affiliate partner)
 2. Prijs van artikel
@@ -9,5 +9,22 @@ Casus: Je hebt 5 onderdelen bij een product:
 4. verzendkosten
 5. unique extern link naar dit product (je affliate link van dit product)
 plaats ze bij de producten
+om idee te krijgen wat we bouwen kun je live voorbeeld zien:
+https://www.kwekersvergelijk.nl/product/pitrus-juncus-effusus/
+
+ Opdracht 1:
+ Zet in een array alle informaties die boven genoemd zijn (bedrijfslogo, prijs, levertijd, verzendkosten, link naar partner).
+ Voorbeeld:
+ var product = [
+                'Pitrus Juncus effusus',
+                'https://www.kwekersvergelijk.nl/wp-content/uploads/2018/08/logo-tuinplant-e1535733140981.png',
+                '31',
+                'gratis',
+                'externlink'
+              ]
+Opdracht 2:
+Plaats alles uit array naar pagina rechts van de product.
+zie voorbeeld:
+https://www.kwekersvergelijk.nl/product/pitrus-juncus-effusus/
 
 */
