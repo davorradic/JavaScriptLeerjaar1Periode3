aa25123e217176180ecc4afde5d11453bb61143d
davorradic <44604843+davorradic@users.noreply.github.com>
2018-12-11 16:35:30 +0100

    alert added

--- opdracht0/js/main.js
+++ opdracht0/js/main.js
@@ -1,5 +1,6 @@
 /* Theorie uit boek: hoofdstuk 1 */
 console.log("voor variabele ---> ");
 var variabele = 'planten zaad';
+alert(variabele);
 console.log(variabele);
 console.log("< --- na variabele"); //string plus string. Mogelijk?
